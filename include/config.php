32d35d92469f2e7f20a4739eee86a80bf79dfb5a
ikramshabri <ikram.shabri@gmail.com>
2017-05-26 21:35:15 +0700

    Create config.php

---
+++ include/config.php
@@ -0,0 +1,10 @@
+<?php
+define("DB_PDODRIVER", "mysql");
+define("DB_HOST", "localhost");
+define("DB_DATABASE", "url");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "");
+
+
+define("SHORTURL_PREFIX", "http://localhost/r.php?c=");
+
